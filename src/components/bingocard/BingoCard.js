ccbbe72b1fb5cf690a717c24963c45a9d6f7c497
Damian <damianobattaglia97@gmail.com>
2023-01-20 20:32:52 +0100

    bingo loop seemingly working

--- src/components/bingocard/BingoCard.js
+++ src/components/bingocard/BingoCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { shuffleArray } from "../../utils/helpers";
 import styled from "styled-components";
 import uniqid from "uniqid";
@@ -6,22 +6,50 @@ import uniqid from "uniqid";
 import material from "../../utils/cardContent";
 import BingoTile from "./BingoTile";
 import FreeTile from "./FreeTile";
+import {
+  checkVerticalBingo,
+  checkHorizontalBingo,
+  checkDiagonalBingo,
+} from "../../utils/verifyBingo";
 
 const BingoCard = () => {
   const [cards, setCards] = useState(shuffleArray(material));
-  const [hit, setHit] = useState([12]); // Always start with 12 as hit
+  const [hit, setHit] = useState([12]); // Always start with 12 as hit;
+  const [last, setLast] = useState(null);
+  useEffect(() => {
+    checkForBingo(last);
+  }, [hit]);
+
+  const checkForBingo = (index) => {
+    const isVerticalBingo = checkVerticalBingo(index, hit)
+    const isHorizontalBingo = checkHorizontalBingo(index, hit)
+    const isDiagonalBingo = checkDiagonalBingo(index, hit)
+    
+    if(isVerticalBingo.result) console.log('It\'s a vertical Bingo!')
+    if(isHorizontalBingo.result) console.log('It\'s a horizontal Bingo!')
+    if(isDiagonalBingo.result) console.log('It\'s a diagonal Bingo!')
+  };
 
   const toggleTile = (index) => {
     const isAlreadyClicked = hit.indexOf(index) !== -1;
-    if(isAlreadyClicked) setHit(hit.filter(number => number !== index))
-    else setHit(prevState => prevState.concat([index]));
-  }
+    if (isAlreadyClicked) setHit(hit.filter((number) => number !== index));
+    else setHit((prevState) => prevState.concat([index]));
+    setLast(index);
+  };
 
   return (
     <BingoGrid>
       {cards.map((item, index) => {
-        if(index === 12) return <FreeTile key={uniqid()}/>
-        return <BingoTile index={index} key={uniqid()} hit={hit} content={item} toggleTile={toggleTile} />;
+        if (index === 12) return <FreeTile key={uniqid()} />;
+        return (
+          <BingoTile
+            index={index}
+            key={uniqid()}
+            hit={hit}
+            content={item}
+            toggleTile={toggleTile}
+          />
+        );
       })}
     </BingoGrid>
   );
---
+++ src/utils/verifyBingo.js
@@ -0,0 +1,68 @@
+const convertToCoord = (index) => {
+  const y = index < 5 ? index : index % 5;
+  const x = index < 5 ? 0 : Math.trunc(index / 5);
+  return [x, y];
+};
+
+const convertToTile = (x, y) =>  (5 * x) + y;
+
+
+const checkVerticalBingo = (hitIndex, hits) => {
+  let counter = 0;
+  const bingo = [];
+  const [x, y] = convertToCoord(hitIndex);
+  for (let i = 0; i < 5; i++) {
+    const index = convertToTile(i, y);
+    if (hits.indexOf(index) !== -1) {
+      counter += 1;
+      bingo.push(index);
+    } else break;
+  }
+  if (counter === 5) return { result: true, bingo };
+  else return { result: false };
+};
+
+const checkHorizontalBingo = (hitIndex, hits) => {
+  let counter = 0;
+  const bingo = [];
+  const [x, y] = convertToCoord(hitIndex);
+  for (let i = 0; i < 5; i++) {
+    const index = convertToTile(x, i);
+    if (hits.indexOf(index) !== -1) counter += 1;
+    else break;
+  }
+  if (counter === 5) return { result: true, bingo };
+  else return { result: false };
+};
+
+const checkDiagonalBingo = (hitIndex, hits) => {
+  const leftDiagonal = [0, 6, 18, 24];
+  const rightDiagonal = [4, 8, 16, 20];
+  const bingo = [];
+  let counter = 0;
+
+  if (leftDiagonal.indexOf(hitIndex) !== -1) {
+    for (let i = 0; i < 5; i++) {
+      const index = convertToTile(i, i);
+      if (hits.indexOf(index) !== -1) {
+        counter += 1;
+        bingo.push(index);
+      } else break;
+    }
+    if (counter === 5) return { result: true, bingo };
+  } else if (rightDiagonal.indexOf(hitIndex) !== -1) {
+    let y = 0;
+    for (let i = 4; i >= 0; i--) {
+      const index = convertToTile(i, y);
+      y += 1;
+      if (hits.indexOf(index) !== -1) {
+        counter += 1;
+        bingo.push(index);
+      } else break;
+    }
+    if (counter === 5) return { result: true, bingo };
+  }
+  return { result: false };
+};
+
+export { checkDiagonalBingo, checkHorizontalBingo, checkVerticalBingo };
